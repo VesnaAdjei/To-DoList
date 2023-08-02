77d1d8d9081ae2c6a3c8c51d64c4bc0642d607b2
Vesna Adjei <vadjei420@gmail.com>
2023-08-02 09:28:49 -0400

    Initial Commit

---
+++ To-DoList/To_DoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_DoListApp.swift
+//  To-DoList
+//
+//  Created by scholar on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct To_DoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
